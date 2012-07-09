1c60a479920873af572fedee61ebe1605f1e33a5
Alejandro Blanco <ablanco@yaco.es>
2012-07-09 12:02:15 +0200

    Password class and list view

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
 /*jslint vars: false, browser: true */
 /*global Em */
 
-var App = Em.Application.create();
+var Yith = Em.Application.create();
 
-App.MyView = Em.View.extend({
-    mouseDown: function () {
-        "use strict";
-        window.alert("hello world!");
-    }
+Yith.passwordList = [];
+
+Yith.Password = Em.Object.extend({
+    service: null,
+    account: null,
+    password: null,
+    expiration: false,
+    notes: null,
+    tags: []
 });
+
+Yith.ListPassordsView = Em.View.extend({
+    passwordList: Yith.passwordList
+});
+
+Yith.passwordList.push(Yith.Password.create({
+    service: "Nyarly",
+    account: "Cultist",
+    password: "this_should_be_ciphered"
+}));
+
+Yith.passwordList.push(Yith.Password.create({
+    service: "Cthulhu",
+    account: "Cultist",
+    password: "this_should_be_ciphered"
+}));
+
+Yith.passwordList.push(Yith.Password.create({
+    service: "Yog",
+    account: "Cultist",
+    password: "this_should_be_ciphered"
+}));
+
+Yith.passwordList.push(Yith.Password.create({
+    service: "Hastur",
+    account: "Cultist",
+    password: "this_should_be_ciphered"
+}));
+
