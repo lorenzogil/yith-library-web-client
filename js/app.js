f243dc1e225a57a0591329bab44adf245ebdf87c
Alejandro Blanco <ablanco@yaco.es>
2012-07-09 13:33:29 +0200

    Modals for viewing notes and editing passwords

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-/*jslint vars: false, browser: true */
-/*global Em */
+/*jslint vars: false, browser: true, nomen: true */
+/*global Em, $ */
 
 var Yith = Em.Application.create();
 
 Yith.passwordList = [];
 
 Yith.Password = Em.Object.extend({
+    _id: null,
     service: null,
     account: null,
     secret: null,
@@ -15,7 +16,30 @@ Yith.Password = Em.Object.extend({
 });
 
 Yith.ListPassordsView = Em.View.extend({
-    passwordList: Yith.passwordList
+    passwordList: Yith.passwordList,
+
+    notes: function (evt) {
+        "use strict";
+        var notes = evt.context.get("notes");
+
+        if (typeof Yith.notesModal === "undefined") {
+            Yith.notesModal = $("#notes");
+            Yith.notesModal.modal({ show: false });
+        }
+        Yith.notesModal.find("p.viewport").text(notes);
+        Yith.notesModal.modal("show");
+    },
+
+    edit: function (evt) {
+        "use strict";
+        var password = evt.context;
+
+        if (typeof Yith.editModal === "undefined") {
+            Yith.editModal = $("#edit");
+            Yith.editModal.modal({ show: false });
+        }
+        Yith.editModal.modal("show");
+    }
 });
 
 Yith.passwordList.push(Yith.Password.create({
@@ -26,9 +50,10 @@ Yith.passwordList.push(Yith.Password.create({
 
 Yith.passwordList.push(Yith.Password.create({
     service: "Cthulhu",
-    account: "Cultist",
+    account: "cultist@rlyeh.com",
     secret: "this_should_be_ciphered",
-    tags: ["scary"]
+    tags: ["scary"],
+    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae erat tortor, ac tincidunt felis. Donec ac libero nunc, eget semper ante. Sed at sapien tellus, nec porttitor nisl. Integer consectetur, risus scelerisque tempus tincidunt, nibh metus sagittis eros, non interdum magna ligula vitae massa. Vestibulum gravida vestibulum diam. Donec sodales, nisi quis ultrices tincidunt, metus turpis scelerisque odio, at feugiat justo urna quis urna. Nullam blandit vehicula urna et vestibulum. Maecenas viverra sem at mauris tincidunt eu laoreet sem ultricies. Cras tincidunt sagittis massa, quis ultricies orci rhoncus ut. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed nunc turpis, consequat eget lacinia pretium, volutpat ac ante. Fusce euismod est ac sapien posuere tristique dignissim justo vehicula. Suspendisse tristique mollis purus, quis vulputate odio ullamcorper id."
 }));
 
 Yith.passwordList.push(Yith.Password.create({
