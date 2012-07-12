558a86fd323b0cbfcb04616d68e9abe4819cc011
Alejandro Blanco <ablanco@yaco.es>
2012-07-12 12:40:07 +0200

    GPL headers and better readme

--- README.rst
+++ README.rst
@@ -1,4 +1,25 @@
 Yith Library web client
 =======================
 
-Web client for Yith Library server.
+Web client for Yith Library server. Written on top of Pyramid and Ember.js
+
+Server: https://github.com/Yaco-Sistemas/yith-library-server
+
+Boring legal stuff
+------------------
+
+Yith Library web client
+Copyright (C) 2012  Yaco Sistemas S.L.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+# Yith Library web client
+# Copyright (C) 2012  Yaco Sistemas S.L.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
     'pyramid',
