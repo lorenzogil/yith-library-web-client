3efc7d67d8a934c00510628e3cffb13bc863d546
Alejandro Blanco <alejandro.b.e@gmail.com>
2012-12-05 19:09:12 +0100

    Force the use of https unless the settings says otherwise

--- runapp.py
+++ runapp.py
@@ -5,6 +5,7 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    scheme = os.environ.get("SCHEME", "https")
     app = loadapp('config:production.ini', relative_to='.')
 
-    serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port, scheme=scheme)
