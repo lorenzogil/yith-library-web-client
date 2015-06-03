3e843b9d0474657eeefc896b06e50968defb2514
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-06-02 21:40:19 -0700

    Fix project name in license section

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,20 @@
-# Yith Library Server is a password storage server.
+# Yith Library Web Client is a client for Yith Library Server.
 # Copyright (C) 2015 Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
 #
-# This file is part of Yith Library Server.
+# This file is part of Yith Library Web Client.
 #
-# Yith Library Server is free software: you can redistribute it and/or modify
+# Yith Library Web Client is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Yith Library Server is distributed in the hope that it will be useful,
+# Yith Library Web Client is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Yith Library Server.  If not, see <http://www.gnu.org/licenses/>.
+# along with Yith Library Web Client.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import os.path
