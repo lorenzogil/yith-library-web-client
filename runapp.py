c67f7bc7b1c6fad4b5ab1d946a35ea91169c36df
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-06-27 20:46:42 -0700

    Redirect to Openshift instance

--- runapp.py
+++ runapp.py
@@ -1,11 +1,17 @@
 import os
 
-from paste.deploy import loadapp
+#from paste.deploy import loadapp
 from waitress import serve
+
+
+def application(environ, start_response):
+    start_response('301 Moved Permanently', [('Location','https://desktop.yithlibrary.com')])
+    return ['']
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     scheme = os.environ.get("SCHEME", "https")
-    app = loadapp('config:production.ini', relative_to='.')
+    #app = loadapp('config:production.ini', relative_to='.')
 
-    serve(app, host='0.0.0.0', port=port, url_scheme=scheme)
+    serve(application, host='0.0.0.0', port=port, url_scheme=scheme)
